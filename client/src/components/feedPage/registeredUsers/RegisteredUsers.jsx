69f9dd5c47a7e9526237b389ac75217d1a2edc75
CvejovicZ95 <cvejovicz@gmail.com>
2024-09-17 12:02:37 +0200

    fixed bug, for sending first message, now no need to refresh page

--- client/src/components/feedPage/registeredUsers/RegisteredUsers.jsx
+++ client/src/components/feedPage/registeredUsers/RegisteredUsers.jsx
@@ -5,12 +5,13 @@ import { CiChat1 } from "react-icons/ci";
 import { useGetUsers } from "../../../hooks/useUsers";
 import { useAuthContext } from "../../../context/authContext";
 import { useUserConversations } from "../../../hooks/useConversations";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 export const RegisteredUsers = () => {
     const { users, loading } = useGetUsers();
     const { authUser } = useAuthContext();
     const { handleCreateConversation } = useUserConversations(authUser?.id);
+    const navigate = useNavigate();
 
     if (loading) {
         return <p>Loading...</p>;
@@ -18,15 +19,14 @@ export const RegisteredUsers = () => {
 
     const filteredUsers = users.filter(user => user.username !== authUser?.username);
 
-    const handleChatClick = async (receiverId) => {
+    const handleChatClick = async (receiverId, username) => {
         try {
             await handleCreateConversation(receiverId);
+            navigate(`/conversation/${username}`); // Navigacija nakon što je konverzacija kreirana
         } catch (error) {
             console.error("Error creating conversation:", error);
         }
     };
-    
-    
 
     return (
         <div className="registered-users">
@@ -46,7 +46,9 @@ export const RegisteredUsers = () => {
                             <FaUserCircle className="user-icon" />
                         )}
                         <p>{user.username}</p>
-                        <button onClick={() => handleChatClick(user._id, user.username)}><Link to={`/conversation/${user.username}`}><CiChat1 className="user-chat-icon"/></Link></button>
+                        <button onClick={() => handleChatClick(user._id, user.username)}>
+                            <CiChat1 className="user-chat-icon"/>
+                        </button>
                     </li>
                 ))}
             </ul>
